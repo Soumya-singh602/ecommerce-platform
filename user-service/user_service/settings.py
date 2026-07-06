b52cf5bbde55e222a4271168e449a2f3001ad15c
Soumya-singh602 <singh.soumya3008@gmail.com>
2026-07-06 13:51:23 +0530

    Connect user service to MySQL

--- user-service/user_service/settings.py
+++ user-service/user_service/settings.py
@@ -73,9 +73,13 @@ WSGI_APPLICATION = 'user_service.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "user_db",
+        "USER": "ecommerce",
+        "PASSWORD": "password123",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
