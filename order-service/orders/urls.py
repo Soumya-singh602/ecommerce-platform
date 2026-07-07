16b5668c4f397cdd80a0fbd31ce2cd31bc394d65
Soumya-singh602 <singh.soumya3008@gmail.com>
2026-07-07 16:19:46 +0530

    Setup order service with basic place order API

---
+++ order-service/orders/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import place_order
+
+urlpatterns = [
+
+    path("", place_order, name="place-order"),
+
+]
--- order-service/orders/views.py
+++ order-service/orders/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .serializers import OrderSerializer
+
+
+@api_view(["POST"])
+def place_order(request):
+
+    serializer = OrderSerializer(data=request.data)
+
+    if serializer.is_valid():
+
+        serializer.save()
+
+        return Response(
+            {
+                "message": "Order Placed Successfully",
+                "data": serializer.data
+            },
+            status=status.HTTP_201_CREATED
+        )
+
+    return Response(
+        serializer.errors,
+        status=status.HTTP_400_BAD_REQUEST
+    )
