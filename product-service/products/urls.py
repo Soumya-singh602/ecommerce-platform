ae850fe09cc97d29b6b4b2f3f952a20ce5353235
Soumya-singh602 <singh.soumya3008@gmail.com>
2026-07-07 11:23:00 +0530

    Add product detail API

--- product-service/products/urls.py
+++ product-service/products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import create_product, product_list
+from .views import create_product, product_list, product_detail
 
 urlpatterns = [
      path("", product_list, name="product-list"),
     path("create/", create_product, name="create-product"),
+     path("<int:id>/", product_detail, name="product-detail"),
 ]
--- product-service/products/views.py
+++ product-service/products/views.py
@@ -8,6 +8,8 @@ from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
 
+from django.shortcuts import get_object_or_404
+
 
 
 
@@ -36,5 +38,14 @@ def product_list(request):
     products = Product.objects.all()
 
     serializer = ProductSerializer(products, many=True)
+
+    return Response(serializer.data)
+
+@api_view(["GET"])
+def product_detail(request, id):
+
+    product = get_object_or_404(Product, id=id)
+
+    serializer = ProductSerializer(product)
 
     return Response(serializer.data)
