57adb8545c24ae3b96df9d4be9a84e253ab3b80c
Soumya-singh602 <singh.soumya3008@gmail.com>
2026-07-07 16:03:18 +0530

    Create Order model

--- order-service/orders/models.py
+++ order-service/orders/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class Order(models.Model):
+
+    user_id = models.IntegerField()
+
+    product_id = models.IntegerField()
+
+    quantity = models.PositiveIntegerField(default=1)
+
+    status = models.CharField(
+        max_length=20,
+        default="Pending"
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order {self.id}"
