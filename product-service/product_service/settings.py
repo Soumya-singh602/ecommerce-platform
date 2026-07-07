dd2944721b1e6f96dfe153c4324c4e4f137e2a17
Soumya-singh602 <singh.soumya3008@gmail.com>
2026-07-07 10:39:17 +0530

    Connect product service to PostgreSQL

--- product-service/product_service/settings.py
+++ product-service/product_service/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'product_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'product_db',
+        'USER': 'product_user',
+        'PASSWORD': 'product123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
