93aa9b0cc25cac4f4434de613b2f237371c0aae0
Soumya-singh602 <singh.soumya3008@gmail.com>
2026-07-07 11:05:24 +0530

    Add product create API

--- product-service/products/views.py
+++ product-service/products/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Product
+from .serializers import ProductSerializer
+
+
+@api_view(["POST"])
+def create_product(request):
+
+    serializer = ProductSerializer(data=request.data)
+
+    if serializer.is_valid():
+
+        serializer.save()
+
+        return Response(
+            {
+                "message": "Product Created Successfully",
+                "data": serializer.data
+            },
+            status=status.HTTP_201_CREATED
+        )
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
