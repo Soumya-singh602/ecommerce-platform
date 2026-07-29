445e2a026cab8762f13020895ca47ab3a41c0c72
Soumya-singh602 <singh.soumya3008@gmail.com>
2026-07-29 16:33:15 +0530

    Prepare product service docker deployment

--- product-service/ecommerce_common/ecommerce_common/utils.py
+++
@@ -1,5 +0,0 @@
-def get_user_info(request):
-    return {
-        "user_id": request.headers.get("X-User-Id"),
-        "user_email": request.headers.get("X-User-Email"),
-    }
--- product-service/ecommerce_common/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="ecommerce-common",
-    version="1.0.0",
-    packages=find_packages(),
-)
