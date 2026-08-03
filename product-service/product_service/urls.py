71aafc81f3b2eda370514749957bde7efd6bd0a0
Soumya-singh602 <singh.soumya3008@gmail.com>
2026-08-03 14:34:17 +0530

    serve media files through django

--- product-service/product_service/urls.py
+++ product-service/product_service/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from products.media_views import serve_media
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("products/", include("products.urls")),
+    path("media/<path:path>", serve_media),
 ]
 
 urlpatterns += static(
