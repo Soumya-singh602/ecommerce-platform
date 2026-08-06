f8d02921e9d0c58efbda94faecf8153c76a48485
Soumya-singh602 <singh.soumya3008@gmail.com>
2026-08-06 15:44:57 +0530

    Fix Stripe publishable key config for card saving

--- frontend/customer-ui/src/main.jsx
+++ frontend/customer-ui/src/main.jsx
@@ -4,33 +4,19 @@ import { BrowserRouter } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
 
 import { Elements } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
+import stripePromise from "./config/stripe";
 
 import "./index.css";
 import App from "./App";
 
-
-const stripePromise = loadStripe(
-    import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY
-);
-
-
 ReactDOM.createRoot(
     document.getElementById("root")
 ).render(
-
     <BrowserRouter>
-
         <Elements stripe={stripePromise}>
-
             <CartProvider>
-
                 <App />
-
             </CartProvider>
-
         </Elements>
-
     </BrowserRouter>
-
 );
